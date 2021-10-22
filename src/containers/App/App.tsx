04a72d02cdc5d5c8c4c2da3982c991ffd5b9a81a
Firmino Changani <firmino.changani@gmail.com>
2021-10-22 23:13:41 +0100

    chore: setup error boundary

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,13 @@
+import { Errors } from "../Errors/Errors";
 import { Routes } from "../Routes/Routes";
 
 export function App() {
   return (
     <main>
-      <Routes></Routes>
+      <h1>Starwars</h1>
+      <Errors>
+        <Routes></Routes>
+      </Errors>
     </main>
   );
 }
