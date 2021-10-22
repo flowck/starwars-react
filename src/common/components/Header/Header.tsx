0489163102483e6bc74635a91ac896ee1d5d45ba
Firmino Changani <firmino.changani@gmail.com>
2021-10-23 00:19:38 +0100

    feat: add header and footer comp

---
+++ src/common/components/Header/Header.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import { Link, NavLink } from "react-router-dom";
+import { Layout } from "@/common/components/Layout/Layout";
+import Heading from "@kiwicom/orbit-components/lib/Heading";
+import TextLink from "@kiwicom/orbit-components/lib/TextLink";
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  background-color: #000;
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    &,
+    span {
+      color: #fff;
+      text-decoration: none;
+    }
+  }
+`;
+
+const Nav = styled.nav`
+  ul {
+    height: 100%;
+    display: flex;
+    list-style: none;
+    align-items: center;
+
+    li {
+      &:not(:last-child) {
+        margin-right: 20px;
+      }
+    }
+  }
+`;
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Layout>
+        <HeaderContent>
+          <Heading inverted type="title1">
+            <Link to="/">starwars</Link>
+          </Heading>
+
+          <Nav>
+            <ul>
+              <li>
+                <NavLink activeClassName="nav--active" to="/people">
+                  <TextLink asComponent="span">Peope</TextLink>
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink activeClassName="nav--active" to="/starships">
+                  <TextLink asComponent="span">Startships</TextLink>
+                </NavLink>
+              </li>
+            </ul>
+          </Nav>
+        </HeaderContent>
+      </Layout>
+    </HeaderContainer>
+  );
+}
